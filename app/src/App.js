80512d898c7fb7ce91f0a7e3f141682de7398634
Ward Chamberlain <wchamberlain89@gmail.com>
2021-12-06 12:56:40 -0800

    Adds ability to mint to fontend

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import CandyMachine from './CandyMachine';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -70,11 +70,13 @@ const App = () => {
         <div className="header-container">
           <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
-          {/* Add the condition to show this only if we don't have a wallet address */}
           {!walletAddress && renderNotConnectedContainer()}
         </div>
+
+        
+        {walletAddress && <CandyMachine walletAddress={window.solana} />}
+
         <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
             className="footer-text"
             href={TWITTER_LINK}
